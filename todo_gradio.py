b9d1d85d53734411362b4a686d8bb403ed194dd8
cdh101890 <cdh101890@gmail.com>
2025-06-17 10:44:05 +0900

    delete 추가

--- todo_gradio.py
+++ todo_gradio.py
@@ -1,3 +1,14 @@
 # todo_gradio.py
 print("Welcome to the Todo List App!")
 print("Let's start!")
+
+
+todos = []
+def add_todo(todo):
+    todos.append(todo)
+    print("Added:",todo)
+
+def delete_todo(todo):
+    if 0 <= index < len(todos):
+        removed = todos.pop(index)
+        print("Deleted:", removed)
